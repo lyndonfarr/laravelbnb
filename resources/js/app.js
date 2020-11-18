3bdae7e8c145d442cfdd08c3094edb86f488878f
Lyndon Farr <lyndonfarr@protonmail.com>
2020-11-18 09:13:27 +0000

    new filter registered, star rating component registered, refactor of registering of index component

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,21 @@ require('./bootstrap');
 import router from './routes';
 import VueRouter from 'vue-router';
 import Index from './Index';
+import moment from 'moment';
+import StarRating from './shared/components/StarRating.vue';
 
 window.Vue = require('vue');
 
 Vue.use(VueRouter);
 
+Vue.filter('fromNow', dateTime => moment(dateTime).fromNow());
+
+Vue.component('star-rating', StarRating);
+
 const app = new Vue({
     el: '#app',
     router,
     components: {
-        'index': Index,
+        Index,
     }
 });
