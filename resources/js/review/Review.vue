6aaf06ef05d2a1f373ef8e67239634813f76aaf2
Lyndon Farr <lyndonfarr@protonmail.com>
2020-12-28 21:51:43 +0000

    functionality added for if there is already an existing review with uuid

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,17 +1,31 @@
 <template>
     <div>
-        <div class="form-group">
-            <label class="text-muted">Select the star rating</label>
-            <star-rating
-                class="fa-3x"
-                v-model="review.rating"
-            ></star-rating>
+        <div v-if="loading">
+            <h3>Loading...</h3>
         </div>
-        <div class="form-group">
-            <label for="content" class="text-muted">Describe your experience with</label>
-            <textarea name="content" cols="30" rows="10" class="form-control"></textarea>
+        <div v-else-if="alreadyReviewed">
+            <h3>You've already left a review for this booking!</h3>
         </div>
-        <button class="btn-lg btn-primary btn-block">Submit</button>
+        <div v-else>
+            <div class="form-group">
+                <label class="text-muted">Select the star rating</label>
+                <star-rating
+                    class="fa-3x"
+                    v-model="review.rating"
+                ></star-rating>
+            </div>
+            <div class="form-group">
+                <label for="content" class="text-muted">Describe your experience with</label>
+                <textarea
+                    name="content"
+                    cols="30"
+                    rows="10"
+                    class="form-control"
+                    v-model="review.content"
+                ></textarea>
+            </div>
+            <button class="btn-lg btn-primary btn-block">Submit</button>
+        </div>
     </div>
 </template>
 
@@ -23,7 +37,25 @@ export default {
                 rating: 5,
                 content: null,
             },
+            existingReview: null,
+            loading: false,
         }
+    },
+
+    computed: {
+        alreadyReviewed() {
+            return !!this.existingReview;
+        },
+    },
+
+    created() {
+        this.loading = true;
+        axios
+            .get(`/api/reviews/${this.$route.params.id}`)
+            .then(res => this.existingReview = res.data.data)
+            .catch(err => {})
+            .then(() => this.loading = false)
+        ;
     },
 }
 </script>
